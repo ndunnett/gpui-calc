f7d29539c8042a08486ef78f2e394dc08d7205fe
ndunnett <nathandunnett@me.com>
2024-05-16 18:30:08 +1000

    build script not really necessary at this point

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    if cfg!(target_os = "macos") {
-        println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET=10.15.7");
-    }
-
-    // if cfg!(target_os = "windows") {
-    //     println!("cargo:rustc-link-arg=/stack:{}", 8 * 1024 * 1024);
-    // }
-}
