6d93ea8ecb3ff3814df14ce8ea06222ab9241578
ndunnett <nathandunnett@me.com>
2024-11-13 02:22:37 +1000

    change window options

--- src/app.rs
+++ src/app.rs
@@ -10,31 +10,36 @@ const WINDOW_SIZE: Size<Pixels> = Size {
     height: px(320.),
 };
 
-const APP_TITLE: &str = "GPUI Calculator";
+const MIN_WINDOW_SIZE: Size<Pixels> = Size {
+    width: px(200.),
+    height: px(280.),
+};
 
-fn get_window_options(bounds: Bounds<Pixels>) -> WindowOptions {
-    WindowOptions {
-        window_bounds: Some(WindowBounds::Windowed(bounds)),
-        titlebar: Some(TitlebarOptions {
-            title: Some(APP_TITLE.into()),
-            appears_transparent: true,
-            ..Default::default()
-        }),
-        focus: true,
-        show: true,
-        window_background: WindowBackgroundAppearance::Blurred,
-        kind: WindowKind::PopUp,
-        ..Default::default()
-    }
-}
+const APP_TITLE: &str = "GPUI Calculator";
 
 pub fn run_app() {
     App::new().with_assets(Assets).run(|cx: &mut AppContext| {
         cx.set_global(Theme::default());
         let _ = Assets.load_fonts(cx);
         StateModel::build(cx);
-        let bounds = Bounds::centered(None, WINDOW_SIZE, cx);
-        let _ = cx.open_window(get_window_options(bounds), Root::build);
+
+        let window_options = WindowOptions {
+            window_bounds: Some(WindowBounds::Windowed(Bounds::centered(
+                None,
+                WINDOW_SIZE,
+                cx,
+            ))),
+            titlebar: Some(TitlebarOptions {
+                title: Some(APP_TITLE.into()),
+                appears_transparent: true,
+                ..Default::default()
+            }),
+            window_background: WindowBackgroundAppearance::Blurred,
+            window_min_size: Some(MIN_WINDOW_SIZE),
+            ..Default::default()
+        };
+
+        let _ = cx.open_window(window_options, Root::build);
         cx.activate(true);
     });
 }
